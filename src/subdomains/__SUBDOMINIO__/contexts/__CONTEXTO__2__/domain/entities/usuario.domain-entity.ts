cb8b1463c79f89eb81b8b03f2f71f0ffd76f3a07
Julian Lasso <julian.lasso@sofka.com.co>
2023-03-03 12:40:30 -0500

    Day 4

--- src/subdomains/__SUBDOMINIO__/contexts/__CONTEXTO__2__/domain/entities/usuario.domain-entity.ts
+++ src/subdomains/__SUBDOMINIO__/contexts/__CONTEXTO__2__/domain/entities/usuario.domain-entity.ts
@@ -2,10 +2,36 @@ import { NombreValueObject } from '../value-objects/usuario/nombre/nombre.value-
 import { UserIdValueObject } from '../value-objects/usuario/usuario-id/usuario-id.value-object';
 import { IUsuarioDomainEntity } from './interfaces/usuario.domain-entity.interface';
 
+/**
+ * Entidad de dominio para el Usuario
+ *
+ * @export
+ * @class UsuarioDomainEntity
+ * @implements {IUsuarioDomainEntity}
+ */
 export class UsuarioDomainEntity implements IUsuarioDomainEntity {
+  /**
+   * Identificador del usuario
+   *
+   * @type {(string | UserIdValueObject)}
+   * @memberof UsuarioDomainEntity
+   */
   userId?: string | UserIdValueObject;
+
+  /**
+   * Nombre del usuario
+   *
+   * @type {(string | NombreValueObject)}
+   * @memberof UsuarioDomainEntity
+   */
   nombre?: string | NombreValueObject;
 
+  /**
+   * Crea una instancia de UsuarioDomainEntity
+   *
+   * @param {IUsuarioDomainEntity} [data] Datos para inicializar la entidad
+   * @memberof UsuarioDomainEntity
+   */
   constructor(data?: IUsuarioDomainEntity) {
     if (data?.userId) this.userId = data.userId;
     if (data?.nombre) this.nombre = data.nombre;

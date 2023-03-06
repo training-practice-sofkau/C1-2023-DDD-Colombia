88ad290c267ffc9f3fc18d1b22d17b93f32d9ff2
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-03-05 23:34:41 -0500

    servicio - realizar servicio de inventorio

---
+++ src/subdomains/clothing-ware-houses/contexts/products/domain/services/inventory.domain-service.ts
@@ -0,0 +1,6 @@
+import { InventoryDomainEntity } from '../entities/inventory/inventory.domain-entity';
+export interface IInventoryDomainService<Entity extends InventoryDomainEntity> {
+  addInventory(): Promise<Entity>;
+  updateAvailableQuantity(): Promise<Entity>;
+  updateMiniumQuantityRequired(): Promise<Entity>;
+}
